19db26b9babc9598bb265eba1ad99bea5de37282
unknown <rampagecopilation@gmail.com>
2022-10-20 16:17:44 -0500

    Ejercicio1-Taller2

---
+++ Taller3-Des-II/ejer1/fichero.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $alumnos = array('Sussie', 'Pepe', 'Alberto', 'Lucas', 'Gianluca');
+    $notas = array(18, 10, 05, 14, 12);
+
+    
+    ?>
+    <center>
+        <table border="1">
+            <tr>
+                <td>Alumno</td>
+                <td>Notas</td>
+                <td>Estado</td>
+            </tr>
+            <tr>
+                <td>
+                    <?php
+                    for($i = 0; $i <5; $i++){
+                        echo $alumnos[$i] . '<br/>';
+                    }
+                    ?>
+                </td>
+                <td>
+                    <?php
+                    for($i = 0; $i <5; $i++){                    
+                        echo $notas[$i] . '<br/>';
+                    }
+                    ?>
+                </td>
+                <td>
+                    <?php
+                    for($i = 0; $i <5; $i++){
+                        if($notas[$i] > 10){
+                            echo 'aprobado';
+                        }elseif($notas[$i] < 11){
+                            echo 'desaprobado';
+                        }
+                        echo '<br/>';
+                    }
+                    ?>
+                </td>
+            </tr>
+        </table>
+    </center>
+</body>
+</html>
